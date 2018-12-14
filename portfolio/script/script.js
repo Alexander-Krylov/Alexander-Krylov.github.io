f02c8fce37e1efc1e1c28d718e79d3aa4615136f
Alexander-Krylov <Alex_kryl2@mail.ru>
2018-12-14 12:43:11 +0300

    fourth-commit

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -7,3 +7,26 @@ $(document).ready(function() {
 		});
 	});
 });
+
+var $btnTop = $('.btn-top');
+
+$(window).on('scroll', function(){
+	if($(window).scrollTop() >= 350){
+		$btnTop.fadeIn();
+	} else {
+		$btnTop.fadeOut();
+	}
+});
+
+$btnTop.on('click', function() {
+	$("html, body").animate({scrollTop: 0}, 750);
+});
+
+if(window.matchMedia("screen and (max-width: 1024px)").matches){
+	$btnTop.html('&#9650;');
+	$btnTop.css({
+		height: '70px',
+		width: '70px',
+		fontSize: '30px'
+	});
+}
